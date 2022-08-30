72e6ff33fd792437167ee57c6a2c8c4df27bcf02
juanevangelista1 <juan.evangelista.nascimentoo@gmail.com>
2022-08-30 09:54:34 -0300

    modified:   src/App.jsx new file:   src/components/Post.jsx new file:   src/components/Post.module.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { SideBar } from './components/Sidebar'
 import { useState } from 'react'
 import { Header } from './components/Header'
+import { Post } from './components/Post'
 import './global.css'
 import styles from './App.module.css'
 
@@ -11,7 +12,7 @@ function App() {
       <div className={styles.wrapper}>
         <SideBar />
         <main>
-          <h5>POSTS</h5>
+          <Post />
         </main>
       </div>
     </>
---
+++ src/components/Post.jsx
@@ -0,0 +1,38 @@
+﻿import styles from './Post.module.css'
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://avatars.githubusercontent.com/u/86501569?v=4"
+            alt="Foto de Perfil"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Juan Evangelista</strong>
+            <span>Web Developerz</span>
+          </div>
+        </div>
+
+        <time title="30 de Agosto às 09:44h" dateTime="2022-08-30 09:43:42">
+          Publicado há uma 1h
+        </time>
+      </header>
+      <div className={styles.content}>
+        <p> Fala galeraa 👋 </p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉 <a href="#"> jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto #nlw #rocketseat</a>{' '}
+        </p>
+      </div>
+    </article>
+  )
+}
